c89e89b91627fcc38da42cd46c042d24dba6a56f
bmit-egg <84566833+bmit-egg@users.noreply.github.com>
2022-05-10 11:17:46 +0200

    Aufgabe 4 fertig

--- Aufgabe_4/Aufgabe 4_1/open_csv.php
+++ Aufgabe_4/Aufgabe 4_1/open_csv.php
@@ -1,15 +1,12 @@
 <html>
     <body>
     <?php
-// Create a table from a csv file 
 echo "<html><body><table>\n\n";
 $f = fopen("Book1.csv", "r");
 while (($line = fgetcsv($f)) !== false) {
-        $row = $line[0];    // We need to get the actual row (it is the first element in a 1-element array)
-        $cells = explode(";",$row);
         echo "<tr>";
-        foreach ($cells as $cell) {
-            echo "<td>" . htmlspecialchars($cell) . "</td>";
+        foreach ($line as $cell) {
+                echo "<td>" . htmlspecialchars($cell) . "</td>";
         }
         echo "</tr>\n";
 }
